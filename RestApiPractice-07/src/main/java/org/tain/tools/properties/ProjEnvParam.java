56139b855416fec00def7919647f7881136a2a36
gksruf2848 <4455qqq@naver.com>
2022-04-05 13:51:19 +0900

    commit

---
+++ RestApiPractice-07/src/main/java/org/tain/tools/properties/ProjEnvParam.java
@@ -0,0 +1,24 @@
+package org.tain.tools.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties(prefix = "proj-env.param")
+@Data
+public class ProjEnvParam {
+
+	private String name;  // default
+	
+	private String home;
+	private String base;
+	private String infoPath;
+	private String dataPath;
+	
+	private String basePath;
+	private String campFile;
+	
+	private String dummy;  // null
+}
