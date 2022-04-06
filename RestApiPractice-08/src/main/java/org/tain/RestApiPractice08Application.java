9642c52dbd84f1bb6989fce7a3026e180a998eeb
gksruf2848 <4455qqq@naver.com>
2022-04-06 11:01:53 +0900

    create project

---
+++ RestApiPractice-08/src/main/java/org/tain/RestApiPractice08Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPractice08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPractice08Application.class, args);
+	}
+
+}
