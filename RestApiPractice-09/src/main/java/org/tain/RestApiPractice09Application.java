9a0acaa0e9259bf42281eeb92dabe6abf084cae3
gksruf2848 <4455qqq@naver.com>
2022-04-06 12:31:02 +0900

    Create commit

---
+++ RestApiPractice-09/src/main/java/org/tain/RestApiPractice09Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPractice09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPractice09Application.class, args);
+	}
+
+}
