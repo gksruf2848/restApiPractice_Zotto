d0e667006c4a1cae65e804d0147d743edecae5f2
gksruf2848 <4455qqq@naver.com>
2022-05-19 15:38:28 +0900

    Create commit

---
+++ RestApiPractice-10-Inbody/src/main/java/org/tain/RestApiPractice10Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPractice10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPractice10Application.class, args);
+	}
+
+}
---
+++ RestApiPractice-10-Inbody/src/test/java/org/tain/RestApiPractice10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPractice10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
